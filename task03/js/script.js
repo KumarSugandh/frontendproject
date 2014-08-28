53ed977d1d626ff59de74a7ae39939a64a80f98b
unknown <kumar_sugandh@.persistent.co.in>
2014-08-28 11:56:37 +0530

    commiting task03

---
+++ task03/js/script.js
@@ -0,0 +1,15 @@
+function one(){
+	return "one (1)";
+}
+function two(){
+	return "two (2)";
+}
+var process;
+
+process = function() {
+  return "Hello There. Same here";
+};
+
+$(function() {
+  return $('h1').append('Hello there, have a great day.');
+});
