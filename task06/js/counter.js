dd404dea186778d89d6a8fa3a2a38c75f5f487f6
unknown <kumar_sugandh@.persistent.co.in>
2014-09-02 15:05:52 +0530

    Committing task06

---
+++ task06/js/counter.js
@@ -0,0 +1,14 @@
+﻿var Counter;
+
+Counter = (function() {
+
+	// private variable
+	var value= 0;
+	
+	// public APIs
+	return {
+		get: function(){return value;},
+		increment: function(){value++;},
+		reset: function(){value=0;}
+	}
+}());
